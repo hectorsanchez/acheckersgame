99a35b6b3a8eda275424be7e604fe49b8ac1c172
hugoruscitti <hugoruscitti@1bfbec1a-f137-0410-bd9a-b1cab423ab52>
2009-03-23 22:43:37 +0000

    Creando objeto King

---
+++ king.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+"""Modulo para manejo de fichas reina"""
+from checker import Checker
+import common
+from config import *
+
+class King(Checker):
+    """Representa una pieza reina del juego.
+
+    Realiza un comportamiento similar a Checker, solo que las
+    finas dama pueden avanzar varios casilleros a la vez.
+    """
+
+    def __init__(self, player, initial_position, table):
+        Checker.__init__(self, player, initial_position, table)
+        self._load_images(player, THEME)
+
+    def _load_images(self, player, theme):
+        """Carga todas las imagenes del tema"""
+        prefix = "p%d_"% player
+        names =  ['normal', 'over', 'drag']
+        filenames = [prefix + name + "_king" + '.png' for name in names]
+        self.images = [common.load_image(name, theme) for name in filenames]
