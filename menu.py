798d779d092d2b5a53814899fed8793ce82e3521
karuchalanux <karuchalanux@1bfbec1a-f137-0410-bd9a-b1cab423ab52>
2010-03-08 01:58:16 +0000

    clase menu

---
+++ menu.py
@@ -0,0 +1,16 @@
+import pygame
+import scene
+
+class Menu(Scene):
+
+    def __init__(self, world):
+        self.world = world
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def on_event(self, event):
+        pass
