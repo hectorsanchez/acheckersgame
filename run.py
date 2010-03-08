e4f97e3e3f258dcf7fe67b12608422966890ade1
hugoruscitti <hugoruscitti@1bfbec1a-f137-0410-bd9a-b1cab423ab52>
2010-03-08 01:58:05 +0000

    actualizando run.

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 # -*- encoding: utf-8 -*-
 from world import *
+import menu
 
 def main():
     world = World(do_center_window=True)
-    world.change_scene(Menu(world))
+    world.change_scene(menu.Menu(world))
 
 if __name__ == '__main__':
     main()
