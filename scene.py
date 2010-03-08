6fc7ed1eff2f9e0c7679e4426ec28c326efd036f
hugoruscitti <hugoruscitti@1bfbec1a-f137-0410-bd9a-b1cab423ab52>
2010-03-08 01:42:41 +0000

    creando la escena abstracta.

---
+++ scene.py
@@ -0,0 +1,13 @@
+class Scene:
+
+    def __init__(self, world):
+        self.world = world
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def on_event(self, event):
+        pass
