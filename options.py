c7011be98a0ad4b178f55b2510b369e6c447cbad
Hugo Ruscitti <hugoruscitti@gmail.com>
2010-08-01 20:32:52 -0300

    fixes #3 - colocando un nuevo menu dentro de la escena options mostrando la opcion pantalla completa y ventanaa

--- options.py
+++ options.py
@@ -5,21 +5,34 @@ from config import *
 from pygame.locals import *
 import common
 import menu
+import menu_base
 
 class Options(scene.Scene):
 
     def __init__(self, world):
         scene.Scene.__init__(self, world)
         self.background = pygame.image.load("ima/options.png")
+        opciones = [
+            ("Pantalla completa / Ventana", self.toggle_fullscreen),
+            ("Regresar", self.return_to_main_menu),
+            ]
+        self.menu = menu_base.MenuBase(opciones, 0)
 
     def update(self):
         pass
 
     def draw(self, screen):
-        screen.blit(self.background, (0, 0))
+        screen.blit(self.background, (0,0))
+        self.menu.draw(screen, self.background)
         pygame.display.flip()
 
     def on_event(self, e):
         if e.type == KEYDOWN:
+            self.menu.on_key_down(e)
             if e.key == K_ESCAPE:
-                self.world.change_scene(menu.Menu(self.world, 1))
+                self.return_to_main_menu()
+
+    def toggle_fullscreen(self):
+        pygame.display.toggle_fullscreen()
+    def return_to_main_menu(self):
+        self.world.change_scene(menu.Menu(self.world, 1))
